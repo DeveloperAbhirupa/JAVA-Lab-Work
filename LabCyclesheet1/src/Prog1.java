9e3419fa4b0c50a262d37d85f5d93499cf316c63
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2019-01-17 22:28:01 +0530

    First Program

---
+++ LabCyclesheet1/src/Prog1.java
@@ -0,0 +1,21 @@
+/**
+ * Write a program to demonstrate the knowledge of students in basic Java concepts.
+ * Eg.Write a program to read the First name and Last name of a person, his weight and height using command line arguments. 
+ * Calculate the BMI Index which is defined as theindividual's body mass divided by the square of their height.
+ */
+
+/**
+ * 
+ * @author Abhirupa MItra 17BCEO437
+ * PROGRAM 1
+ */
+import java.io.*;
+
+public class Prog1 {
+    
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+        
+    }
+}
