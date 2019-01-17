01c14973e4dc6b120791372b04cfee91493b2b87
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2019-01-17 22:46:21 +0530

    Prog1 completed

--- LabCyclesheet1/src/Prog1.java
+++ LabCyclesheet1/src/Prog1.java
@@ -16,6 +16,33 @@ public class Prog1 {
     public static void main(String args[])throws IOException
     {
         BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
+        String firstName, lastName;
+        double weight,height;
+        System.out.println("Enter first name:");
+        firstName=br.readLine();
+        System.out.println("Enter last name:");
+        lastName=br.readLine();
+        System.out.println("Enter height: (in cm)");
+        height=Double.parseDouble(br.readLine());
+        System.out.println("Enter weight:");
+        weight=Double.parseDouble(br.readLine());
+        double bmi= weight/(height*height);
+        if(bmi<18.5)
+            System.out.println("Underweight");
+        else if(bmi<=25)
+            System.out.println("Normal(healthy weight");
+        else if(bmi<=30)
+            System.out.println("Overweight");
+        else
+            System.out.println("Obese Class");
+    }
+}
+        
+        
+        
+        
+        
+        
         
     }
 }
